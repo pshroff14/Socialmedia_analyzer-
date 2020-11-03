4c29c84ea29ed581757c690f598d2bd855fa8993
Poras Shroff <porasshroff@gmail.com>
2020-11-02 22:42:27 -0500

    Update Analyzer.py

--- Analyzer.py
+++ Analyzer.py
@@ -2,26 +2,20 @@
 #DataFraming from LucidProgramming https://www.youtube.com/watch?v=WX0MDddgpA4
 #Twitter API library from https://github.com/bear/python-twitter
 import twitter
-import requests
-from google.cloud import language
-from google.cloud.language import enums, types
-import pandas #for data framing
+import tweepy
+import json
+# from google.cloud import enum
+from google.cloud import storage
+import google.oauth2.credentials
+from google.cloud import language_v1
 
+# from google.cloud import types
+# from searchtweets import collect_results
 
-
-
-r=requests.get('https://cloud.google.com/natural-language/docs/reference/rest/v1/documents/analyzeSentiment#http-request')
-
-
-
-from searchtweets import collect_results
-
-
-consumer_key = 'Enter Here'
-consumer_secret = 'Enter Here'
-access_token = 'Enter Here'
-access_secret = 'Enter Here'
-
+consumer_key = 'KZczaCiFdUlLJkmautw7YGxf8'
+consumer_secret = 'qyNgBrYrX0YC6qfHg4QDRENKNcAZQ6k47HCZEID1QKhlpOUeOW'
+access_token = '1292600738456248326-DzFIuKrFAZGjYCXJcJx5Z9qF22dkAW'
+access_secret = 'cvbj6jWExK7QUlunhmmz6mUJpfxs5awtSVr0vhxC6riwM'
 
 # Authenticate to Twitter
 api = twitter.Api(consumer_key=consumer_key,
@@ -29,19 +23,48 @@ api = twitter.Api(consumer_key=consumer_key,
                   access_token_key=access_token,
                   access_token_secret=access_secret)
 
-#Authentication to Google NLP
-from gcloud.aio.auth import IamClient
-from gcloud.rest.auth import Token
-client = IamClient()
-pubkeys = await client.list_public_keys()
-token = Token()
-print(token.get())
-print("-----------------")
+# Authenticate to Google
+import google.oauth2.credentials
+
+credentials = google.oauth2.credentials.Credentials(
+    'bPU6i9SaTJeOBn23ogPaG9ok')
 
 
-(api.VerifyCredentials())
+# Get Trending Tweets from Specific Location Based on WOEID
+def trend_location(woeid=0):
+    # all = []
+    if woeid == 0:
+        woeid = 2367105
+    tweet = api.GetTrendsWoeid(woeid, exclude=None)
+    # print(tweet)
+    return tweet
 
-#Sentiment Function
+
+def add(num):
+    result = 1 + num
+    return result
+
+
+# def clean_tweet(self, tweet):
+#     return ' '.join(re.sub("(@[A-Za-z0-9]+)|([^0-9A-Za-z \t])|(\w+:\/\/\S+)", " ", tweet).split())
+# print(clean_tweet(trend_location(0)))
+
+
+# # TopTrendsinBoston (or any WOEID entry) LINK for WOEID:
+# # https://gist.githubusercontent.com/lukemelia/353493/raw/98749866fce79b591e45fb3325c853b4306a8882/WOEIDs%2520for
+# # %2520US%2520Cities%2520with%2520population%2520over%2520100K%2520as%2520of%25202008%2520(from%2520Wikipedia)
+# woeid = 0
+# print("ENTER WOEID OF CITY:   ", )
+# input(woeid)
+# if woeid == 0:
+#     woeid = 2367105
+# atrend = api.GetTrendsWoeid(woeid, exclude=None)
+# print("TOP TRENDING IN WOEID CITY:   ", atrend)
+
+print("---------------------------------------------------")
+
+
+# Sentiment Function
 def analyze_text_sentiment(text):
     client = language.LanguageServiceClient()
     document = types.Document(
@@ -58,27 +81,6 @@ def analyze_text_sentiment(text):
     ]
     for k, v in results:
         print('{:10}: {}'.format(k, v))
-
-#Json tweet to data frame function
-def tweets_to_data_frame(self,tweets):
-    df = pd.DataFrame(data=[tweet.text for tweet in tweets], columns=['Tweets'])
-    return df
-
-
-
-#GetCurrentTrends
-gtrends=api.GetTrendsCurrent(exclude=None)
-print("Current Global Trends:  " ,gtrends)
-print("---------------------------------------------------")
-
-#data framing
-df = tweets_to_data_frame(gtrends)
-
-#Sentiment of GetCurrentTrends from the data framing
-analyze_text_sentiment(df)
-
-
-
 
 
 
