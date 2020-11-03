872402ae38b5fba4f20d2ac9f4682751d720b829
Poras Shroff <porasshroff@gmail.com>
2020-11-02 23:11:55 -0500

    Add files via upload

---
+++ test_Analyzer.py
@@ -0,0 +1,20 @@
+import unittest
+from Analyzer import *
+
+
+class TestTwit(unittest.TestCase):
+
+    # def test_twitauth(self):
+    #     self.assertisNone(trend_location(0))
+
+    def test_add(self):
+        self.assertEqual(add_num(5),6)
+
+
+
+
+
+if __name__ == '__main__':
+    unittest.main()
+
+
