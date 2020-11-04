272e3e13245ba2c6afcf4fe8c000985d8ae9f5f8
Poras Shroff <porasshroff@gmail.com>
2020-11-04 16:25:56 -0500

    Add files via upload

--- test_Analyzer.py
+++ test_Analyzer.py
@@ -10,6 +10,17 @@ class TestTwit(unittest.TestCase):
     def test_add(self):
         self.assertEqual(add_num(5),6)
 
+    # def test_auth(self):
+    #     self.assertEqual()
+
+# This test see's if the list of strings of the GettrendsWoeid is the same as the function itself
+    def test_woe(self):
+        woeid = 23637105    # Enter WOEID number
+        a = trend_location(woeid)
+        b = api.GetTrendsWoeid(woeid, exclude=None)
+        self.assertListEqual(a, b)
+
+
 
 
 
